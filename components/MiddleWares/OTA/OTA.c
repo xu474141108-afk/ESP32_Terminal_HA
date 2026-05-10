da5037a70d47e82f0a0250e5ae27cd2be2f8741d
Hellochigo <xu474141108@gmail.com>
2026-05-10 17:46:03 +0200

    OTA modificado

--- components/MiddleWares/OTA/OTA.c
+++ components/MiddleWares/OTA/OTA.c
@@ -1,7 +1,18 @@
-#include "lvgl.h"
+
+#include <string.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_event.h"
 #include "esp_log.h"
 #include "esp_ota_ops.h"
+#include "esp_app_format.h"
 #include "esp_http_client.h"
+#include "esp_flash_partitions.h"
+#include "esp_partition.h"
+#include "nvs.h"
+#include "nvs_flash.h"
 
 #define WIFI_CONNECTION_ERROR              (1 << 0)
 
